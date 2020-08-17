c12b665914d826d42e22e65cb0c11b023f59594c
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2020-08-17 00:22:33 -0400

    View Ok and controller in progress

---
+++ calculator/ICommand.swift
@@ -0,0 +1,25 @@
+//
+//  ICommand.swift
+//  calculator
+//
+//  Created by Helwan Mandé on 2020-08-16.
+//  Copyright © 2020 Helwan Mandé. All rights reserved.
+//
+
+import Foundation
+
+class ICommand {
+    
+    init(){
+        
+        
+    }
+    //func execute()->Double{}
+    
+}
+
+class CommandMultiply : ICommand{
+  
+}
+
+
--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,8 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    var performingMath:Bool = false
+    var numberOnScreeen:Double = 0;
+    var result:Double?
+    var operation:String?
+    var lastNumber:Int?
+    @IBOutlet weak var label: UILabel!
+    
+    
+    //Interacting with numbers
+    @IBAction func numbers(_ sender: UIButton) {
+        
+        label.text = label.text! + String(sender.tag-1)
+        
+        if (performingMath == false){
+        self.numberOnScreeen = Double(label.text!)!
+        }
+        
+        lastNumber = sender.tag-1
+    }
+    
+    
+    //Interacting with *
+    @IBAction func multiply(_ sender: UIButton) {
+        
+        label.text = "x"
+        operation = "x"
+        performingMath = true
+        
+      
+        
+    }
+    
+    ////Interacting with equals
+    @IBAction func equals(_ sender: UIButton) {
+        
+        switch operation {
+        case "x":
+            result = Double(lastNumber!)*numberOnScreeen
+        case "+":
+            result=Double(lastNumber!)+numberOnScreeen
+        case "-":
+            result=Double(lastNumber!)-numberOnScreeen
+        case "/":
+            result=Double(lastNumber!)/numberOnScreeen
+        default:result = 0
+            
+        }
+        
+        label.text = String(result!)
+        performingMath = false
+        
+    }
+    
+    
+    
+    @IBAction func clear(_ sender: UIButton) {
+        numberOnScreeen = 0
+        lastNumber = 0
+        operation=""
+        label.text=""
+        
+        
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
