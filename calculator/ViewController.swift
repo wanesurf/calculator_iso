1ddffc691ae4c9e722cdacefc3e653fc843355d3
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2020-08-16 21:55:29 -0400

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Helwan Mandé on 2020-08-16.
+//  Copyright © 2020 Helwan Mandé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
